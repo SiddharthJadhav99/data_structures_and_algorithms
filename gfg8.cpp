0d52a5a2470be9e30c0f15dc175fe4f197c5e266
Siddharth Jadhav <jadhavsid99@gmail.com>
2021-07-04 14:59:25 +0530

    Multidimensional Array using Pointer - Static and Dynamic memory allocation

--- gfg8.cpp
+++ gfg8.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int m = 3, n = 2;
-    int **ptr;
-    ptr = new int* [m]; //dynamic memory alloted in heap
+    int m = 3;
+    int n =2;
+    int *ptr[m]; //static memory allocation - stored in stack
     for (int i = 0; i < m; i++){
-        ptr[i] = new int [n]; //dynamic memory allotment
+        ptr[i] = new int [n]; //dynamic memory allocation - stored in heap
     }
     for (int i = 0; i < m; i++){
         for (int j = 0; j < n; j++){
             ptr[i][j] = 10;
             cout<<ptr[i][j]<<"\t";
         }
+        cout<<endl;
     }
 }
