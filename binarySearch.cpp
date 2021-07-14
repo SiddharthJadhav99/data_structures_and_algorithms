51c6c66038cbc7e7d1ec53dca762ba00ed20d216
Siddharth Jadhav <jadhavsid99@gmail.com>
2021-07-14 09:46:07 +0530

    Updated Binary Search

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int binarySearch(vector<int> &arr, int n){
-    for (int i = 0; i < arr.size(); i++){
-        if(arr[i] == n){
-            return i;
+int binarySearch(vector<int> &arr, int n, int low, int high){
+    while(low <= high){
+        int mid = (low + high)/2;
+        if (arr[mid] == n){
+            return mid;
+        }
+        else if (arr[mid] > n){
+           high = mid-1;
+        }
+        else if(arr[mid] < n){
+            low = mid+1;
         }
     }
     return -1;
@@ -14,7 +21,9 @@ int main(){
     vector<int> arr = {1,2,4,6,7,9,10};
     cout<<"Enter the number to be searched : ";
     cin>>n;
-    int index = binarySearch(arr, n);
+    int low = 0;
+    int high = arr.size()-1;
+    int index = binarySearch(arr, n, low, high);
     cout<<"The index at which the number is present is: "<<index;
     return 0;
 }
