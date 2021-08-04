4d8f3288ba94d226a00cf95e657c290a9d9d1ad1
Siddharth Jadhav <jadhavsid99@gmail.com>
2021-08-04 23:57:58 +0530

    Bubble Sort

---
+++ Sorting/bubbleSort.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void bubbleSort(vector<int> &arr, int n){
+    for (int i = 0; i < n-1; i++){
+        for (int j = 0; j < n-i-1; j++){
+            if (arr[j] > arr[j+1]){
+                int temp;
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+int main(){
+    vector<int> arr = {10,8,20,25};
+    int n = arr.size();
+    bubbleSort(arr,n);
+    cout<<"<===Sorted Array===>"<<endl;
+    for (int x : arr){
+        cout<<x<<"\t";
+    }
+    return 0;
+}
