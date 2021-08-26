0be905164a618e043fca3f9168b75e16106eb097
Siddharth Jadhav <jadhavsid99@gmail.com>
2021-08-26 21:52:39 +0530

    Delete head of Circular Linked List

---
+++ Circular Linked List/6deleteHeadLL.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+struct node{
+    int data;
+    node* next;
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+node* deleteHead(node* head){
+    node* curr = head;
+    while(curr->next != head){
+        curr = curr->next;
+    }
+    curr->next = head->next;
+    delete(head);
+    return curr->next;
+}
+
+void printLL(node* head){
+    node* temp = head;
+    do{
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }while(temp != head);
+}
+
+int main(){
+    node* head = new node(20);
+    head->next = new node(30);
+    head->next->next = new node(40);
+    head->next->next->next = head;
+    node* new_head = deleteHead(head);
+    printLL(new_head);
+    return 0;
+}
