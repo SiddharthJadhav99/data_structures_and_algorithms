90b5f9d40da7ef1b3c988672c42df2a2b3eb8eea
Siddharth Jadhav <jadhavsid99@gmail.com>
2021-08-26 17:17:13 +0530

    Insert at Beginning

---
+++ Circular Linked List/2insertAtBeginning.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+struct node{
+    int data;
+    node *next;
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+node* insertAtBegin(node* head, int num){
+    node* curr = head;
+    node* temp = new node(num);
+    while(curr->next != NULL){
+       curr = curr->next;
+    }
+    curr->next = temp;
+    temp->next = head;
+    return temp;
+}
+void printLL(node* head){
+    node* temp = head;
+    do{
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }while(temp != head);
+}
+int main(){
+    int num;
+    node* head = new node(10);
+    head->next = new node(20);
+    head->next->next = new node(30);
+    cout<<"Enter the number to Insert: ";
+    cin>>num;
+    node* new_head = insertAtBegin(head, num);
+    printLL(new_head);
+    return 0;
+}
