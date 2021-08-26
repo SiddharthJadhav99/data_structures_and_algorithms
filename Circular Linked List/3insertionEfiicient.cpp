20d1b0c75047235ce26366a9c4e9d979f3c73920
Siddharth Jadhav <jadhavsid99@gmail.com>
2021-08-26 17:35:30 +0530

    Insertion Efficient

---
+++ Circular Linked List/3insertionEfiicient.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+struct node{
+    int data;
+    node* next;
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+node* insertEff(node* head, int num){
+    node* temp = new node(num);
+    if (head == NULL){
+        temp->next = temp;
+        return temp;
+    }
+    else{
+        temp->next = head->next;
+        head->next = temp;
+        int t = head->data;
+        head->data = temp->data;
+        temp->data = t;
+        return head;
+    }
+}
+void printLL(node* head){
+    node* temp = head;
+    do{
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }while(temp != head);
+}
+int main(){
+    int num;
+    node* head = new node(10);
+    head->next = new node(20);
+    head->next->next = new node(30);
+    head->next->next->next = head;
+    cout<<"Enter the numbe to insert: ";
+    cin>>num;
+    node* new_head = insertEff(head, num);
+    printLL(head);
+    return 0;
+}
