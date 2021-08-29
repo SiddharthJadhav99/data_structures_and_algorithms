b68720ba1f2f817c6396f6cbecaf3a2aa87b10a6
Siddharth Jadhav <jadhavsid99@gmail.com>
2021-08-29 22:06:08 +0530

    Stack Using Linked List

---
+++ Stack/3stackUsingLinkedList.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+struct node{
+    int data;
+    node* next;
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+struct MyStack{
+    node* head;
+    int size;
+    MyStack(){
+        head = NULL;
+        size = 0;
+    }
+    void push(int x){
+        node* temp = new node(x);
+        temp->next = head;
+        head = temp;
+        size++;
+    }
+    int pop(){
+        if (head == NULL){
+            return INT_MAX;
+        }
+        int res = head->data;
+        node* temp = head;
+        head = head->next;
+        delete(temp);
+        size--;
+        return res;
+    }
+    int peek(){
+        if(head == NULL){
+            return INT_MAX;
+        }
+        return head->data;
+    }
+    int sized(){
+        return size;
+    }
+    bool isEmpty(){
+        return (head == NULL);
+    }
+};
+int main(){
+    MyStack s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    cout<<s.pop()<<endl;
+    cout<<s.sized()<<endl;
+    cout<<s.peek()<<endl;
+    s.push(25);
+    cout<<s.peek()<<endl;
+    cout<<s.sized()<<endl;
+    return 0;
+}
