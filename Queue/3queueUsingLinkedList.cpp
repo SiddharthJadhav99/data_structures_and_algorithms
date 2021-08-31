ec0b2174699eb28f968bbe5d8474cfd87daca20a
Siddharth Jadhav <jadhavsid99@gmail.com>
2021-08-31 09:33:24 +0530

    Queue Using Linked List

---
+++ Queue/3queueUsingLinkedList.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+struct node{
+    int data;
+    node* next;
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+struct MyQueue{
+    node* head;
+    node* rear;
+    int size;
+    MyQueue(){
+        rear = head;
+        size = 0;
+    }
+    bool isEmpty(){
+        return (size == 0);
+    }
+    void enQueue(int x){
+        node* temp = new node(x);
+        if (isEmpty()){
+            head = temp;
+            rear = temp;
+            size++;
+        }
+        else{
+            rear->next = temp;
+            rear = temp;
+            size++;
+        }
+    }
+    int deQueue(){
+        if(isEmpty()){return;}
+        int res = head->data;
+        node* temp = head;
+        head = head->next;
+        delete(temp);
+        return res;
+        size--;
+    }
+    int size(){
+        return size;
+    }
+    int getFront(){
+        return head->data;
+    }
+    int getRear(){
+        return rear->data;
+    }
+
+};
