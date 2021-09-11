9aa969f3dca8582be3837dfd1e89bf897ea6612a
Siddharth Jadhav <jadhavsid99@gmail.com>
2021-09-11 10:20:01 +0530

    Deque

--- Deque/1arrayImplementationOfDeque.cpp
+++ Deque/1arrayImplementationOfDeque.cpp
@@ -1,12 +1,58 @@
 #include <iostream>
 using namespace std;
-struct Deque{
+struct DQ{
     int size;
     int *arr;
     int cap;
-    Deque(int c){
+    DQ(int c){
         cap = c;
         size = 0;
         arr= new int[c];
     }
+    bool isEmpty(){
+        if (size == 0){return true;}
+    }
+    bool isFull(){
+        if(size == cap){return true;}
+    }
+    void insertFront(int x){
+        if (isEmpty){
+            arr[size] = x;
+            size++;
+        }
+        if(isFull){return;}
+        for(i = size-1; i >= 0; i--){
+            arr[i+1] = arr[i];
+        }
+        arr[0] = x;
+        size = size+1;
+    }
+    void deleteFront(){
+        if(isEmpty){return;}
+        for (i = 0; i < size-1; i++){
+            arr[i] = arr[i+1];
+        }
+        size = size-1;
+    }
+    void insertRear(int x){
+        if(isFull){return;}
+        arr[size] = x;
+        size++;
+    }
+    void deleteRear(){
+        if(isEmpty){return;}
+        size--;
+    }
+    int getFront(){
+        if(isEmpty){
+            return;
+        }
+        return arr[0];
+    }
+    int getRear(){
+        if(isEmpty){return;}
+        return arr[size-1]
+    }
+
+
 };
