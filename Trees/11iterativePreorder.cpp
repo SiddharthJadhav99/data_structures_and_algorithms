8b8ed5d058827d4d7473f3e38914ffb2cf76083f
Siddharth Jadhav <jadhavsid99@gmail.com>
2021-09-12 17:32:25 +0530

    Optimized

--- Trees/11iterativePreorder.cpp
+++ Trees/11iterativePreorder.cpp
@@ -31,7 +31,24 @@ void iterativePreorder(node* root){
 
     }
 }
-
+void optimizedIterativePO(node* root){
+    if (root == NULL){return;}
+    stack<node*> s;
+    node* curr = root;
+    while(curr != NULL || s.empty() == false){
+        while(curr != NULL){
+            cout<<curr->key<<" ";
+            if(curr->right != NULL){
+                s.push(curr->right);
+            }
+            curr = curr->left;
+        }
+        if(s.empty() == false){
+            curr = s.top();
+            s.pop();
+        }
+    }
+}
 int main(){
     node* root = new node(10);
     root->left = new node(20);
