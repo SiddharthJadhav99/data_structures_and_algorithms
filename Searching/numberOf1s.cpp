f155f460da45c7a72f7d68a9e99661e1a112ed45
Siddharth Jadhav <jadhavsid99@gmail.com>
2021-09-29 10:13:38 +0530

    Number of 1's

--- Searching/numberOf1s.cpp
+++ Searching/numberOf1s.cpp
@@ -5,20 +5,29 @@ int numberOfOne(int *arr, int n){
     int start = 0;
     int last = n-1;
     int mid = 0;
+    int count = 0;
+    int r = 0;
     while(start <= last){
         mid = (start + last) / 2;
-        if(arr[mid] == 1){
-            last = mid - 1;
-        }else{
-            start = mid + 1;
+        if(arr[mid] < 1){
+            start = mid+1;
+        }
+        else{
+            if(arr[mid] != arr[mid-1]){
+                r = mid;
+                break;
+            }
+            else{
+                last = mid-1;
+            }
         }
     }
-    return start;
+    count = n-r;
+    return count;
 }
 int main(){
-    int n;
-    cin>>n;
-    int arr[n];
+    int n = 7;
+    int arr[] = {0,0,0,0,1,1,1};
     int number = numberOfOne(arr,n);
     cout<<number;
     return 0;
